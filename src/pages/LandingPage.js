bde847f31382c260d95145bdc7f8acb581e75d7c
Wahyudin <whydn.id@hotmail.com>
2020-07-20 19:31:43 +0700

    :sparkles: Creating Most Picked section arranged to Landing Page

--- src/pages/LandingPage.js
+++ src/pages/LandingPage.js
@@ -2,14 +2,22 @@ import React, { Component } from "react";
 
 import Header from "parts/Header";
 import Hero from "parts/Hero";
+import MostPicked from "parts/MostPicked";
+
 import data from "json/landingPage";
 
 export default class LandingPage extends Component {
+  constructor(props) {
+    super(props);
+    this.refMostPicked = React.createRef();
+  }
+
   render() {
     return (
       <>
-        <Header {...this.props}></Header>
-        <Hero data={data.hero} />
+        <Header {...this.props} />
+        <Hero refMostPicked={this.refMostPicked} data={data.hero} />
+        <MostPicked refMostPicked={this.refMostPicked} data={data.mostPicked} />
       </>
     );
   }
