1978a87fc9c813b57a57cace6b2432c52c09bfad
Wahyudin <whydn.id@hotmail.com>
2020-07-24 23:09:09 +0700

    :bug: Fix date cannot select & picked

--- src/elements/Form/InputDateRange/index.js
+++ src/elements/Form/InputDateRange/index.js
@@ -8,31 +8,19 @@ import "react-date-range/dist/theme/default.css"; // Theme CSS file
 import { dateFormat } from "utils/helpers";
 
 export default function InputDateRange(props) {
-  const { value, placeholder, name, className } = props;
+  const { value, placeholder, name } = props;
   const [isShowed, setIsShowed] = useState(false);
   const refDate = useRef(null);
 
-  const handleOnChange = (value) => {
+  const datePickerChange = (value) => {
     const target = {
-      value: value.selection,
-      name: name,
+      target: {
+        value: value.selection,
+        name: name,
+      },
     };
     props.onChange(target);
   };
-
-  const handleClickOutside = (event) => {
-    if (refDate && refDate.current.contains(event.target)) {
-      setIsShowed(false);
-    }
-  };
-
-  const check = (focus) => {
-    focus.indexOf(1) < 0 && setIsShowed(false);
-  };
-
-  const displayDate = `${
-    value.startDate ? dateFormat(value.startDate) : ""
-  } - ${value.endDate ? dateFormat(value.endDate) : ""}`;
 
   useEffect(() => {
     document.addEventListener("mousedown", handleClickOutside);
@@ -42,12 +30,29 @@ export default function InputDateRange(props) {
     };
   });
 
+  const handleClickOutside = (event) => {
+    if (refDate && !refDate.current.contains(event.target)) {
+      setIsShowed(false);
+    }
+  };
+
+  const check = (focus) => {
+    focus.indexOf(1) < 0 && setIsShowed(false);
+  };
+
+  const displayDate = `${value.startDate ? dateFormat(value.startDate) : ""}${
+    value.endDate ? " - " + dateFormat(value.endDate) : ""
+  }`;
+
   return (
-    <div ref={refDate} className={["input-date mb-3", className].join(" ")}>
+    <div
+      ref={refDate}
+      className={["input-date mb-3", props.className].join(" ")}
+    >
       <div className="input-group">
         <div className="input-group-prepend bg-gray-900">
           <span className="input-group-text">
-            <img src="icons/icon-calendar.svg" alt="Icon Calendar" />
+            <img src="/icons/icon-calendar.svg" alt="icon calendar" />
           </span>
         </div>
         <input
@@ -58,11 +63,12 @@ export default function InputDateRange(props) {
           placeholder={placeholder}
           onClick={() => setIsShowed(!isShowed)}
         />
+
         {isShowed && (
           <div className="date-range-wrapper">
             <DateRange
               editableDateInputs={true}
-              onChange={handleOnChange}
+              onChange={datePickerChange}
               moveRangeOnFirstSelection={false}
               onRangeFocusChange={check}
               ranges={[value]}
